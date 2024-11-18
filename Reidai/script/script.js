7f44e5ad5a9c2ab929cbc271d4bf882e0e03c831
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-11-18 10:45:54 +0900

    例題の修正

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,16 +1,22 @@
 // JavaScript用ファイル
 new Vue({
     el: '#app',
-    
-    data: {
+    data:{
+      password:'',
+      confirmPassword:''
     },
     computed: {
+      passwordMismatch(){
+        return this.password && this.confirmPassword &&
+          this.password !==this.confirmPassword;
+      },
       // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
       // 入力されているかは、this.･･･ で判定可能
       // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
       // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
-
+      canRegister(){
+        return this.password && this.confirmPassword && !this.passwordMismatch;
+      }
       // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
     }
   });
